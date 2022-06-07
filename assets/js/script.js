5058e1d754f8c9278e6baf1f01bd51d1044fa804
rfabreu <raabreugomes@gmail.com>
2022-06-07 10:52:14 -0400

    Capture Form Field Values

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function (event) {
     // Tell the browswer not to follow its default behavior of updating the page
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     // Create a list item
     var listItemEl = document.createElement("li");
     // Assign a class name to apply CSS style to the item created
     listItemEl.className = "task-item";
-    // Insert text content in the item created
-    listItemEl.textContent = "This is a new task.";
-    // Insert the item created into the HTML document
+
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
